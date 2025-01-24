287449ca7900fed8739e60894056304e5d6953f3
codewithshorty <codewithshorty@gmail.com>
2025-01-24 14:27:40 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TechNews from "./components/TechNews";
+
+const App = () => {
+  return (
+    <div>
+      <TechNews />
+    </div>
+  );
+};
+
+export default App;
